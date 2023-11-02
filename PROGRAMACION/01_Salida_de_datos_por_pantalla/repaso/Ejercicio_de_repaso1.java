f5a351dde9a65ad5d56802d00de7ff35df5c3fd5
Miguel Ángel Ávila Rosas <148963870+MiguelAngelAvilaRosas1DAW@users.noreply.github.com>
2023-11-02 13:22:49 +0100

    Delete PROGRAMACION directory

--- PROGRAMACION/01_Salida_de_datos_por_pantalla/repaso/Ejercicio_de_repaso1.java
+++
@@ -1,9 +0,0 @@
-public class Ejercicio_de_repaso1 {
-  public static void main(String[] args) {
-    System.out.printf("\033[32m%7d\n", 1);
-    System.out.printf("\033[33m%-5d5\n", 2);
-    System.out.printf("\033[34m%03d\n", 3);
-    System.out.printf("\033[35m%+4d\033[0m\n", 4);
-    System.out.print("\"Hola\' \u266A\n");
-  }
-}
--- PROGRAMACION/02_Variables/ejercicios/Ejercicio2.java
+++
@@ -1,7 +0,0 @@
-public class Ejercicio2 {
-  public static void main(String[] args) {
-    
-    String nombre = "Miguel Ángel Ávila Rosas";
-    System.out.println(nombre);
-  }
-}
