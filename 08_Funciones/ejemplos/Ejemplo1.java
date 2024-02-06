b56fc1c873a77d386b130246a3d306f54caaece9
Miguel Ángel Ávila Rosas <148963870+MiguelAngelAvilaRosas1DAW@users.noreply.github.com>
2024-02-06 20:47:47 +0100

    Delete 08_Funciones directory

--- 08_Funciones/ejemplos/Ejemplo1.java
+++
@@ -1,7 +0,0 @@
-package ejemplos;
-import java.util.Scanner;
-public class Ejemplo1 {
-  public static void main(String[] args) {
-    
-  }
-}
--- 08_Funciones/ejercicios/Ejercicio00.java
+++
@@ -1,7 +0,0 @@
-import libreria.Matematicas;
-
-public class Ejercicio00 {
-  public static void main(String[] args) {
-    Matematicas.leerValor();
-  }
-}
